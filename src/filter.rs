b689731d35440cda1c64ce105d84e16c0281a99d
Matthias Beyer <mail@beyermatthias.de>
2019-10-26 19:47:42 +0200

    Add filter extension where filter can fail.
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/filter.rs
+++ src/filter.rs
@@ -12,6 +12,10 @@ pub trait Filter<O, E>: Sized {
     fn filter_err<F>(self, F) -> FilterErr<Self, F>
     where
         F: FnMut(&E) -> bool;
+
+    fn filter_ok_and_then<F>(self, F) -> FilterOkAndThenImpl<Self, F>
+    where
+        F: FnMut(&O) -> Result<bool, E>;
 }
 
 impl<I, O, E> Filter<O, E> for I
@@ -29,6 +33,15 @@ where
         F: FnMut(&E) -> bool,
     {
         FilterErr { iter: self, f }
+    }
+
+    /// Extension for `Iterator<Item = Result<O, E>>` to filter the Ok(_) and leaving the Err(_) as
+    /// is, but allowing the filter to return a `Result<bool, E>` itself
+    fn filter_ok_and_then<F>(self, f: F) -> FilterOkAndThenImpl<Self, F>
+    where
+        F: FnMut(&O) -> Result<bool, E>,
+    {
+        FilterOkAndThenImpl { iter: self, f }
     }
 }
 
@@ -102,6 +115,40 @@ where
     }
 }
 
+#[must_use = "iterator adaptors are lazy and do nothing unless consumed"]
+pub struct FilterOkAndThenImpl<I, F> {
+    iter: I,
+    f: F,
+}
+
+impl<I, O, E, F> Iterator for FilterOkAndThenImpl<I, F>
+where
+    I: Iterator<Item = Result<O, E>>,
+    F: FnMut(&O) -> Result<bool, E>,
+{
+    type Item = Result<O, E>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.iter.next() {
+                Some(Ok(x)) => match (self.f)(&x) {
+                    Ok(true)  => return Some(Ok(x)),
+                    Ok(false) => continue,
+                    Err(e)    => return Some(Err(e))
+                }
+
+                other => return other,
+            }
+        }
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let hint_sup = self.iter.size_hint().1;
+        (0, hint_sup)
+    }
+}
+
 #[test]
 fn test_filter_ok() {
     use std::str::FromStr;
@@ -155,3 +202,19 @@ fn test_filter_err_hint() {
 
     assert_eq!(hint, (0, Some(5)));
 }
+
+#[test]
+fn test_filter_ok_and_then() {
+    use std::str::FromStr;
+
+    let v = ["1", "2", "a", "4", "5"]
+        .into_iter()
+        .map(Ok)
+        .filter_ok_and_then(|e| usize::from_str(e).map(|txt| txt < 3))
+        .collect::<Vec<Result<_, _>>>();
+
+    assert_eq!(v.len(), 3);
+    assert_eq!(v.iter().filter(|x| x.is_ok()).count(), 2);
+    assert_eq!(v.iter().filter(|x| x.is_err()).count(), 1);
+}
+
