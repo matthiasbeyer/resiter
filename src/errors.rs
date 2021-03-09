matthiasbeyer/resiter

commit 2cb9f214ffe2e42d21515a89388d89bda66d5d36

Add no_std support

src/errors.rs
@@ -4,7 +4,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
-use std::iter::*;
+use core::iter::*;
 
 use util::*;
 

src/lib.rs
@@ -194,6 +194,9 @@
 //! MPL 2.0
 //!
 
+#![cfg(not(test))]
+#![no_std]
+
 pub mod and_then;
 pub mod errors;
 pub mod filter;

src/oks.rs
@@ -4,7 +4,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
-use std::iter::*;
+use core::iter::*;
 
 use util::*;
 

Cargo.toml
@@ -7,6 +7,7 @@ authors = ["Matthias Beyer <mail@beyermatthias.de>",
 description = "Helper crate for handling iterators over result"
 
 keywords    = ["iterator", "result", "util"]
+categories  = ["no-std"]
 readme      = "README.md"
 license     = "MPL-2.0"
 

commit d2d76da711ebb046ce4f4443f69e2c34886ef840

Fix tests with no_std support
This fixes commit 2cb9f214ffe2e42d21515a89388d89bda66d5d36 where no_std
support was added, but because the !#[no_std] macro was used for the
complete crate no matter what, the crate was essentially empty.

I noticed that during the implementation of a new helper, because no
tests were running.

Fixes: 2cb9f21 ("Add no_std support")
CC: Oleksii Filonenko <brightone@protonmail.com>
Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

src/errors.rs
@@ -4,7 +4,10 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
+#[cfg(not(test))]
 use core::iter::*;
+#[cfg(test)]
+use std::iter::*;
 
 use util::*;
 

src/lib.rs
@@ -176,8 +176,7 @@
 //! MPL 2.0
 //!
 
-#![cfg(not(test))]
-#![no_std]
+#![cfg_attr(not(test), no_std)]
 
 pub mod and_then;
 pub mod errors;

src/oks.rs
@@ -4,7 +4,10 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 //
 
+#[cfg(not(test))]
 use core::iter::*;
+#[cfg(test)]
+use std::iter::*;
 
 use util::*;
 