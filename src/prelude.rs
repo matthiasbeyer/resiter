f53dac31b02f680621cc3f4fcbf343f276f6c9b7
Joakim Ohlrogge <joakim.ohlrogge@gmail.com>
2023-04-25 12:49:16 +0200

    Add missing public uses in prelude
    Signed-off-by: johlrogge <joakim.ohlrogge@agical.se>

--- src/prelude.rs
+++ src/prelude.rs
@@ -9,8 +9,19 @@
 //! Imports all things publicly, so you can `use resiter::prelude::*;` in your crate.
 //!
 
+pub use and_then::*;
 pub use errors::*;
+pub use filter::*;
+pub use filter_map::*;
+pub use flat_map::*;
+pub use flatten::*;
+pub use map::*;
+pub use ok_or_else::*;
 pub use oks::*;
 pub use onerr::*;
 pub use onok::*;
+pub use try_filter::*;
+pub use try_filter_map::*;
+pub use try_map::*;
 pub use unwrap::*;
+pub use while_ok::*;
